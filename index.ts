6486d59dc62aef03125b4b7b13dfdc3e4d06d687
yukikayuki <yukikayuki@gmail.com>
2023-10-15 14:23:08 +0900

    feat: add express app

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import express from 'express'
+const app = express()
+const DEFAULT_PORT = 3000
+
+app.get('/', (req, res) => {
+  return res.json({hello: 'world'})
+})
+
+app.get('/hello', (req, res) => {
+  return res.json({world: 'world'})
+})
+
+app.listen(process.env.PORT ?? DEFAULT_PORT, () => {
+  console.log(`App listening on port ${process.env.PORT ?? DEFAULT_PORT}`)
+})
