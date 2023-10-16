d4638a215b07223fd4e2e9fc2f7c07206934d840
yukikayuki <yukikayuki@gmail.com>
2023-10-16 21:44:17 +0900

    build: use PROXY_TARGET_ADDRESS of environment value

---
+++ sidecar/envoy/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+echo "Generating envoy.yaml config file..."
+cat /tmpl/envoy.tmpl.yaml | envsubst \$PROXY_TARGET_ADDRESS > /etc/envoy.yaml
+
+echo "Starting Envoy..."
+/usr/local/bin/envoy -c /etc/envoy.yaml
